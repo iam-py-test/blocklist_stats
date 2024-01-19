a24b524bae84220aebced969d6855fad858007c0
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2024-01-19 17:24:06 -0500

    Add code

---
+++ plot_stats.py
@@ -0,0 +1,47 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+import matplotlib.colors as allowedcolors
+import json
+import os
+
+filterlists = json.loads(open('filterlists.json').read())
+
+try:
+    stats = json.loads(open('stats.json', 'r', encoding='utf-8').read())
+except:
+    stats = {}
+
+for filter in filterlists:
+    try:
+        fcontents = requests.get(filterlists[filter]).text.replace("\r\n", "\n").split("\n")
+        if filter not in stats:
+            stats[filter] = []
+        numfilters = 0
+        done = []
+        for l in fcontents:
+            if l == "" or l.startswith("#") or l.startswith("!") or l in done:
+                continue
+            done.append(l)
+            numfilters += 1
+        stats[filter].append(numfilters)
+    except:
+        pass
+
+for filter in stats:
+    x = np.arange(1,len(stats[filter]) + 1)
+    y = np.array(stats[filter])
+
+    plt.title(f"Number of unique filters in {filter}")
+    plt.xlabel("Time")
+    plt.ylabel("Filters")
+    plt.plot(x, y, color ="green")
+    plt.savefig(f"stats/{filter}.png")
+    plt.clf()
+
+try:
+    outstats = open("stats.json", 'w')
+    outstats.write(json.dumps(stats))
+    outstats.close()
+except Exception as err:
+    print(err)
+
