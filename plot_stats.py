81a4cb69cc6eca90f1a4b1c0beb4081977ea5d63
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2024-02-11 19:03:49 -0500

    Switch to a date-based format This means one can look at the state of a filterlist at a specific date

--- plot_stats.py
+++ plot_stats.py
@@ -6,8 +6,21 @@ import os
 import requests
 import threading
 import hashlib
+import datetime
 
 filterlists = json.loads(open('filterlists.json').read())
+
+current_date = datetime.datetime.now().strftime("%d/%m/%Y")
+
+def dict_as_arr(dic):
+    """
+    Return the items in a dict as a list (called an array here, don't sue me)
+    I am sure there is a better way
+    """
+    arr = []
+    for item in dic:
+        arr.append(dic[item])
+    return arr
 
 try:
     os.mkdir("stats")
@@ -49,13 +62,13 @@ def count_filters(filter, trust_line_count=False, exclude_from_line_count=0):
         fhash = hashlib.md5(freq.content).hexdigest()
         fcontents = freq.text.replace("\r\n", "\n").split("\n")
         if filter not in stats:
-            stats[filter] = []
+            stats[filter] = {}
         if filter not in change_stats:
-            change_stats[filter] = []
+            change_stats[filter] = {}
         if filter not in size_stats:
-            size_stats[filter] = []
-        change_stats[filter].append(fhash)
-        size_stats[filter].append(len(freq.content))
+            size_stats[filter] = {}
+        change_stats[filter][current_date] = fhash
+        size_stats[filter][current_date] = len(freq.content)
         numfilters = 0
         done = []
         if trust_line_count:
@@ -66,7 +79,7 @@ def count_filters(filter, trust_line_count=False, exclude_from_line_count=0):
                     continue
                 done.append(l)
                 numfilters += 1
-        stats[filter].append(numfilters)
+        stats[filter][current_date] = numfilters
     except Exception as err:
         print(err)
     running_threads -= 1
@@ -86,8 +99,9 @@ while running_threads > 0:
     pass
 
 for filter in stats:
-    x = np.arange(1,len(stats[filter]) + 1)
-    y = np.array(stats[filter])
+    filter_items = dict_as_arr(stats[filter])
+    x = np.arange(1,len(filter_items) + 1)
+    y = np.array(filter_items)
 
     filtername = filter.replace(" ","_").replace("'","").replace("+","_")
     plt.title(f"Number of unique filters in {filter}")
@@ -98,8 +112,9 @@ for filter in stats:
     plt.clf()
 
 for filter in size_stats:
-    x = np.arange(1,len(size_stats[filter]) + 1)
-    y = np.array(size_stats[filter])
+    filter_items = dict_as_arr(size_stats[filter])
+    x = np.arange(1,len(filter_items) + 1)
+    y = np.array(filter_items)
 
     filtername = filter.replace(" ","_").replace("'","").replace("+","_")
     plt.title(f"Size (in bytes) of {filter}")
