Fix filtername normalization

plot_stats.py
@@ -107,7 +107,7 @@ for filter in stats:
     x = np.arange(1,len(filter_items) + 1)
     y = np.array(filter_items)
 
-    filtername = filter.replace(" ","_").replace("'","").replace("+","_").replace(":", "").replace("*", "").replace("&", "")
+    filtername = filter.replace(" ","_").replace("'","").replace("+","_").replace(":", "").replace("*", "").replace("&", "_").replace("/","_")
     plt.title(f"Number of unique filters in {filter}")
     plt.xlabel("Time")
     plt.ylabel("Filters")
@@ -120,7 +120,7 @@ for filter in size_stats:
     x = np.arange(1,len(filter_items) + 1)
     y = np.array(filter_items)
 
-    filtername = filter.replace(" ","_").replace("'","").replace("+","_").replace(":", "").replace("*", "").replace("/","_")
+    filtername = filter.replace(" ","_").replace("'","").replace("+","_").replace(":", "").replace("*", "").replace("&", "_").replace("/","_")
     plt.title(f"Size (in bytes) of {filter}")
     plt.xlabel("Time")
     plt.ylabel("Size")